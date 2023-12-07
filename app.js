ef92b25123f63309e2760749e5586cd937d111e6
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-07 21:12:14 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import data from "./questions.json" assert {type: "json"}
+
+const question = document.querySelector(".question")
+const answerOption = document.querySelector(".answerOptions")
+const nextBtn = document.querySelector(".next_btn")
+
+console.log(question);
