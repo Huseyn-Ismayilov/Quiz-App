91fa646cbe5736dc7f0214728b2666b9750fc767
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-08 00:11:42 +0400

    #

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 import data from "./questions.json" assert {type: "json"}
 
-
-const question_text = document.querySelector(".question")
-const answerOptions = document.querySelector(".answerOptions")
+const questionText = document.querySelector(".question")
+const answerOptionList = document.querySelector(".answerOptions")
+const answerOption = document.querySelector(".answerOptions .option")
 const nextBtn = document.querySelector(".next_btn")
 
 // console.log(data[0].question);
 
 
-data.forEach(function (element) {
-    // console.log(element)
-    // console.log(element.question)
-    question_text.innerHTML = element.question
-})
+// data.forEach(function (element) {
+//     // console.log(element)
+//     // console.log(element.question)
+//     questionText.innerHTML = element.question
+// })
 
 
 
-function nextQuestion() {
+function currentQuestion(data) {
+    // questionText.innerHTML = data.question
+    // console.log( questionText.innerHTML = data.question);
+    data.forEach()
+}
+currentQuestion()
+
+function selectQuestion() {
 
 }
 
-function currentQuestion() {
-    
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
