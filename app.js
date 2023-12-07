1e569acb45346e5f3c68131a59c63db0ce03e062
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-08 01:56:48 +0400

    #3

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 import data from "./questions.json" assert {type: "json"}
 
+const container = document.querySelector(".questionContainer")
 const questionText = document.querySelector(".question")
 const answerOptionList = document.querySelector(".answerOptions")
-const nextBtn = document.querySelector(".next_btn")
+const nextBtn = document.querySelector(".nextBtn")
 
 // console.log(data[0].question);
 
 const currentQuestionIndex = 0
 
 function createQuestion() {
-    let currentQuestionElement = data[currentQuestionIndex]
-    let questionNo = currentQuestionIndex + 1
+    const currentQuestionElement = data[currentQuestionIndex]
+    const questionNo = currentQuestionIndex + 1
     questionText.innerHTML = `${questionNo}. ${currentQuestionElement.question}`
+    
+
+    const time = document.createElement("div")
+    time.classList.add("time")
+    time.innerHTML= `<span>${questionNo}</span>/${data.length}`
+    container.prepend(time)
 
     // console.log(currentQuestionElement.answer_options);
+    // console.log(currentQuestionElement.question);
+
     currentQuestionElement.answer_options.forEach(answer => {
-        const answerButton = document.createElement('li')
+        const answerButton = document.createElement("li")
         answerButton.classList.add("option")
         answerButton.innerHTML = answer
         answerOptionList.appendChild(answerButton)
     })
-    console.log(currentQuestionElement.answer_options)
+    // console.log(currentQuestionElement.answer_options)
+
 }
 createQuestion()
 
+
+nextBtn.addEventListener("click", selectQuestion)
+
 function selectQuestion() {
-
+    currentQuestionIndex = currentQuestionIndex + 1
 }
-
