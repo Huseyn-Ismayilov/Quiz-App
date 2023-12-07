c7cf41712b5ab704bbbdbf5b5a6da65b04aad940
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-08 02:50:39 +0400

    #

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 import data from "./questions.json" assert {type: "json"}
 
 const container = document.querySelector(".questionContainer")
+const time = document.querySelector(".time")
 const questionText = document.querySelector(".question")
 const answerOptionList = document.querySelector(".answerOptions")
 const nextBtn = document.querySelector(".nextBtn")
+
 
 // console.log(data[0].question);
 
 let currentIndex = 0
 
 function createQuestion() {
+    answerOptionList.innerHTML = ""
     const currentQuestionElement = data[currentIndex]
     const questionNo = currentIndex + 1
     questionText.innerHTML = `${questionNo}. ${currentQuestionElement.question}`
 
-
-    const time = document.createElement("div")
-    time.classList.add("time")
     time.innerHTML = `<span>${questionNo}</span>/${data.length}`
-    container.prepend(time)
 
     // console.log(currentQuestionElement.answer_options);
     // console.log(currentQuestionElement.question);
@@ -29,17 +28,15 @@ function createQuestion() {
         answerButton.innerHTML = answer
         answerOptionList.appendChild(answerButton)
     })
-    // console.log(currentQuestionElement.answer_options)
 }
-
 createQuestion()
 
+nextBtn.addEventListener("click", nextQuestion)
 
-nextBtn.addEventListener("click", selectQuestion)
-
-function selectQuestion() {
-
+function nextQuestion() {
     currentIndex++
+    console.log(currentIndex);
+
     if (currentIndex < data.length) {
         createQuestion()
     }
