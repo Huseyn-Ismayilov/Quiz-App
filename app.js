cada9f7122b510933014d15b77be4b84cda81d14
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-08 00:12:04 +0400

    #

--- app.js
+++ app.js
@@ -29,14 +29,5 @@ function selectQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+  
 }
